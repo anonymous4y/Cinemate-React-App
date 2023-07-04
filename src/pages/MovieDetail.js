bfa08ed9a3e448ffbe73943030ffdb3715987262
Harsh Kumar <39022528+anonymous4y@users.noreply.github.com>
2023-07-04 23:23:07 +0530

    MovieDetail.js

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -12,7 +12,7 @@ export const MovieDetail = () => {
 
     useEffect(() => {
         async function fetchMovie() {
-            const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key=c975973c6c42e5f674af87cea79cfab7`)
+            const response = await fetch(`https://api.themoviedb.org/3/movie/${params.id}?api_key= ${process.env.REACT_APP_API_KEY}`)
             const json = await response.json()
             setMovie(json)
         }
